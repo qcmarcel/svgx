bb1d323ad697ec51b7e996aa1f17a358398ec9b7
dev <devnetworksoft@gmail.com>
2024-10-28 14:34:47 -0500

    SVGX v1.0.4
    remove debug logs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 import {_process, ioFile, script} from "./lib/svgx/main.js";
 
 const [_,__,base,...pattern]=process.argv
-console.debug(process.argv, base, pattern)
+// console.debug(process.argv, base, pattern)
 const svgPathPattern = pattern.map((path)=>`${base}/${path}`)
 script.read((script_err, script_data)=>{
     if (script_err) {
